ead87466c3cc6606c44bddb69b1406c679fc0046
Yamuna <yamunarathod779@gmail.com>
2024-04-24 16:53:16 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to render the index.ejs template
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
